4e239be5032f2293bea42f1dc0809e869b78fc7a
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-09-20 12:37:00 +0900

    feat: Add shuffled images for banner

--- dashboard/src/components/Banner/index.jsx
+++ dashboard/src/components/Banner/index.jsx
@@ -7,13 +7,38 @@ import { SEQUENCER_EXPLORER, truncateText } from '../../pages/Dashboard'
 const { Meta } = Card;
 
   
-const Banner = ({ images, speed = 100000 }) => {
+const Banner = ({ images, speed = 50000 }) => {
+
+    // Helper function to shuffle an array
+    const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  };
+  
+  // Function to get doubled shuffled images
+  const getDoubledShuffledImages = (images) => {
+    // Shuffle images and remove duplicates based on their 'name' property
+    let shuffled = shuffleArray(images).filter((imageObj, index, self) =>
+      index === self.findIndex((t) => t.name === imageObj.name)
+    );
+    
+    // Concatenate the shuffled array with itself
+    const doubledShuffledImages = [...shuffled, ...shuffled];
+    
+    return doubledShuffledImages;
+  };
+
+  const doubledShuffledImages = getDoubledShuffledImages(images);
+
     // console.log(images)
     return (
       <div className="inner">
         <div className="wrapper">
           <section style={{ "--speed": `${speed}ms` }}>
-            {images.map(({ id, image, name, desc = "", address = "" }) => (
+            {doubledShuffledImages.map(({ id, image, name, desc = "", address = "" }) => (
             <Link to={SEQUENCER_EXPLORER + address}>
                 <Col style={{ height: 150 }}>
                     <Card
@@ -25,7 +50,7 @@ const Banner = ({ images, speed = 100000 }) => {
                     cover={<img alt={id} src={image} style={{ width: "150px", height: "100px", objectFit: "cover" }} />}
                     className="criclebox"
                     >
-                    <Meta title={name} description={truncateText(desc, 100)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
+                    <Meta title={name} description={truncateText(desc, 80)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
                     </Card>
                 </Col>
             </Link>
@@ -33,7 +58,7 @@ const Banner = ({ images, speed = 100000 }) => {
             ))}
           </section>
           <section style={{ "--speed": `${speed}ms` }}>
-            {images.map(({ id, image, name, desc = "", address = "" }) => (
+            {doubledShuffledImages.map(({ id, image, name, desc = "", address = "" }) => (
             <Link to={SEQUENCER_EXPLORER + address}>
                 <Col style={{ height: 150 }}>
                     <Card
@@ -45,7 +70,7 @@ const Banner = ({ images, speed = 100000 }) => {
                     cover={<img alt={id} src={image} style={{ width: "150px", height: "100px", objectFit: "cover" }} />}
                     className="criclebox"
                     >
-                    <Meta title={name} description={truncateText(desc, 100)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
+                    <Meta title={name} description={truncateText(desc, 80)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
                     </Card>
                 </Col>
             </Link>
@@ -53,7 +78,7 @@ const Banner = ({ images, speed = 100000 }) => {
             ))}
           </section>
           <section style={{ "--speed": `${speed}ms` }}>
-            {images.map(({ id, image, name, desc = "", address = "" }) => (
+            {doubledShuffledImages.map(({ id, image, name, desc = "", address = "" }) => (
             <Link to={SEQUENCER_EXPLORER + address}>
                 <Col style={{ height: 150 }}>
                     <Card
@@ -65,7 +90,7 @@ const Banner = ({ images, speed = 100000 }) => {
                     cover={<img alt={id} src={image} style={{ width: "150px", height: "100px", objectFit: "cover" }} />}
                     className="criclebox"
                     >
-                    <Meta title={name} description={truncateText(desc, 100)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
+                    <Meta title={name} description={truncateText(desc, 80)} style={{ fontSize: 8 }} /> {/* Adjusted font size */}
                     </Card>
                 </Col>
             </Link>
